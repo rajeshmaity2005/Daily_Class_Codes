a588593294e66804e18186f403faece89a7b9f4a
Rajesh Maity <rajeshloq2005@gmail.com>
2026-02-07 19:55:01 +0530

    DOM3

--- script.js
+++ script.js
@@ -6,16 +6,42 @@
 //     h1.style.color = 'brown'
 // })
 
-var h1 = document.querySelector('h1')
-var inc = document.querySelector('#inc')
-var dec = document.querySelector('#dec')
+// var h1 = document.querySelector('h1')
+// var inc = document.querySelector('#inc')
+// var dec = document.querySelector('#dec')
 
-var a = 0;
-inc.addEventListener('click', function () {
-    a++;
-    h1.innerHTML = a
-})
-dec.addEventListener('click', function () {
-    a--;
-    h1.innerHTML = a
+// var a = 0;
+// inc.addEventListener('click', function () {
+//     a++;
+//     h1.innerHTML = a
+// })
+// dec.addEventListener('click', function () {
+//     a--;
+//     h1.innerHTML = a
+// })
+
+
+// var bulb = document.querySelector('#bulb')
+// var btn = document.querySelector('button')
+
+// let flag = 0;
+// btn.addEventListener('click', function () {
+
+//     if (flag == 0) {
+//         bulb.style.backgroundColor = 'yellow'
+//         console.log("heueuehd");
+//         flag = 1;
+//     }
+//     else {
+//         bulb.style.backgroundColor = 'transparent'
+//         console.log("bgbgggnggnheueuehd");
+//         flag = 0;
+//     }
+// })
+
+var h = document.querySelectorAll('h1')
+console.log(h);
+
+h.forEach(function (e) {
+    console.log(e)
 })
