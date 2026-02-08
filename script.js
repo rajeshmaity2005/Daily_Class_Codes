898ca01cd053b8c6628e63bab3dff39512ee3136
Rajesh Maity <rajeshloq2005@gmail.com>
2026-02-08 19:54:04 +0530

    Add Friend Feature with same button

--- script.js
+++ script.js
@@ -1,47 +1,24 @@
-// var h1 = document.querySelector('h1')
-// var btn = document.querySelector('button')
-// btn.addEventListener('click', function () {
-//     // console.log('Hello Brother')
-//     h1.innerHTML = 'Hello Brother'
-//     h1.style.color = 'brown'
-// })
+var stranger = document.querySelector('h5')
+var btn = document.querySelector('button')
 
-// var h1 = document.querySelector('h1')
-// var inc = document.querySelector('#inc')
-// var dec = document.querySelector('#dec')
+var flag = 0;
 
-// var a = 0;
-// inc.addEventListener('click', function () {
-//     a++;
-//     h1.innerHTML = a
-// })
-// dec.addEventListener('click', function () {
-//     a--;
-//     h1.innerHTML = a
-// })
+btn.addEventListener('click', function () {
 
 
-// var bulb = document.querySelector('#bulb')
-// var btn = document.querySelector('button')
-
-// let flag = 0;
-// btn.addEventListener('click', function () {
-
-//     if (flag == 0) {
-//         bulb.style.backgroundColor = 'yellow'
-//         console.log("heueuehd");
-//         flag = 1;
-//     }
-//     else {
-//         bulb.style.backgroundColor = 'transparent'
-//         console.log("bgbgggnggnheueuehd");
-//         flag = 0;
-//     }
-// })
-
-var h = document.querySelectorAll('h1')
-console.log(h);
-
-h.forEach(function (e) {
-    console.log(e)
+    if (flag == 0) {
+        stranger.style.color = 'green'
+        stranger.innerHTML = 'Friend'
+        btn.textContent = 'Remove Friend'
+        flag = 1;
+        btn.style.backgroundColor = 'rgb(206, 206, 206)'
+        btn.style.color = 'white'
+    } else {
+        stranger.style.color = 'red'
+        stranger.innerHTML = 'Stranger'
+        flag = 0;
+        btn.textContent = 'Add Friend'
+        btn.style.backgroundColor = 'cornflowerBlue'
+        btn.style.color = 'white'
+    }
 })
