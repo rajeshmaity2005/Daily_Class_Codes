4de828dbe718209f7ae813612cf8b4f95882cbf3
Rajesh Maity <rajeshloq2005@gmail.com>
2026-02-10 00:11:05 +0530

    Multiple Image Hovering Animation

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-var main = document.querySelector('#main')
-var crsr = document.querySelector('.cursor')
+var elem = document.querySelectorAll('.elem')
 
-main.addEventListener('mousemove', function (dets) {
-    crsr.style.left = dets.x + "px"
-    crsr.style.top = dets.y + "px"
+elem.forEach(function (val) {
+    val.addEventListener('mouseenter', function () {
+        val.childNodes[3].style.opacity = 1
+    })
+    val.addEventListener('mouseleave', function () {
+        val.childNodes[3].style.opacity = 0
+    })
+    val.addEventListener('mousemove', function (dets) {
+        val.childNodes[3].style.left = dets.x + 'px'
+        val.childNodes[3].style.top = dets.y + 'px'
+    })
 })
+
+
+
+
+
+
+
+// var elem1 = document.querySelector('#elem1')
+// var elemImage = document.querySelector("#elem1 img")
+
+// elem1.addEventListener("mousemove", function (dets) {
+//     elemImage.style.left = dets.x + 'px'
+//     elemImage.style.top = dets.y + 'px'
+// })
+
+// elem1.addEventListener("mousemove", function (dets) {
+//     elemImage.style.opacity = 1
+// })
+
+// elem1.addEventListener("mouseleave", function (dets) {
+//     elemImage.style.opacity = 0
+// })
