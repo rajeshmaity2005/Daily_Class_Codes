32cc1300670a7cf6a5efd4d5c51c280399ccd6c4
Rajesh Maity <rajeshloq2005@gmail.com>
2026-02-09 21:21:04 +0530

    Custom Cursor

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
-var con = document.querySelector('#container')
-var love = document.querySelector("i")
+var main = document.querySelector('#main')
+var crsr = document.querySelector('.cursor')
 
-con.addEventListener("dblclick", function () {
-    love.style.transform = 'translate(-50%,-50%) scale(1)'
-    love.style.opacity = 0.8
-
-    setTimeout(function(){
-        love.style.opacity = 0
-    },1000)
-    setTimeout(function(){
-        love.style.transform = 'translate(-50%,-50%) scale(0)'
-    },2000)
+main.addEventListener('mousemove', function (dets) {
+    crsr.style.left = dets.x + "px"
+    crsr.style.top = dets.y + "px"
 })
