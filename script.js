847a05c2671546a0ad15bc7b07fbda805b14f9bf
Rajesh Maity <rajeshloq2005@gmail.com>
2026-03-05 12:29:51 +0530

    Creating an element

--- script.js
+++ script.js
@@ -1,10 +1,59 @@
+// var btn = document.querySelector('button')
+// var main = document.querySelector('main')
+// btn.addEventListener('click', function () {
+//     var div = document.createElement('div')
+
+//     var x = Math.random()*100
+//     var y = Math.random()*100
+//     var r = Math.random()*360
+    
+//     var c1 = Math.floor(Math.random()*256)
+//     var c2 = Math.floor(Math.random()*256)
+//     var c3 = Math.floor(Math.random()*256)
+
+//     div.style.height='50px'
+//     div.style.width='50px'
+//     div.style.position='absolute'
+//     div.style.backgroundColor=`rgb(${c1},${c2},${c3})`
+    
+//     div.style.left=x+'%'
+//     div.style.top=y+'%'
+//     div.style.rotate=r+'deg'
+
+//     main.appendChild(div)
+// })
+
+
+
+
 var btn = document.querySelector('button')
-var box = document.querySelector('#box')
+var main = document.querySelector('main')
 
-btn.addEventListener('click', function(){
-    var a = Math.floor(Math.random()*256)
-    var b = Math.floor(Math.random()*256)
-    var c = Math.floor(Math.random()*256)
+var arr= ['Hey! I am Rajesh','Sheryians is Best','Learn and Study Ahead','Anubhav is Handsme','Hello Everyone','js is love','Learn everything','Keep practising']
 
-    box.style.backgroundColor = `rgb(${a}, ${b}, ${c})`
+btn.addEventListener('click',function(){
+    var h1 = document.createElement('h1')
+
+    var a = Math.floor(Math.random()*arr.length)
+    var x = Math.random()*80
+    var y = Math.random()*80
+    var r = Math.random()*360
+    var scl = Math.random()*3
+
+    var c1 = Math.floor(Math.random()*256)
+    var c2 = Math.floor(Math.random()*256)
+    var c3 = Math.floor(Math.random()*256)
+
+    h1.innerHTML=arr[a]
+    
+    h1.style.position='absolute'
+    h1.style.left=x+'%'
+    h1.style.top=y+'%'
+    h1.style.rotate=r+'deg'
+    h1.style.scale=scl
+
+    // h1.style.color=`rgb(${c1},${c2},${c3})`
+
+    main.appendChild(h1)
+
 })
---
+++ task.md
@@ -0,0 +1,16 @@
+Topics 
+
+createElement is used to create new element
+appendChild is used to append it in any parent.
+
+1. Make 'main tag in HTML, give it height and width 100%
+2. Make a button and center it. (give it position relative and z-index)
+3. In JS select button and main div and add a eventListener on button
+4. Inside a function create new element with the help of document.createElement method.
+5. If you have successfully created an element print it on console to check it.
+6. Write innerHTML if you want 
+7. Use parent.appendChild method to add this new element in main div.
+
+Task - 
+
+When user click on button cretae h1, put random quote in it (from array) and give random position, rotation, scale, color(optional) and append them to parent
