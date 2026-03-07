314ef675e0afcb754ec8c541de85856f22c4e7e2
Rajesh Maity <rajeshloq2005@gmail.com>
2026-03-07 10:46:17 +0530

    timeout (Download Button)

--- script.js
+++ script.js
@@ -1,59 +1,32 @@
-// var btn = document.querySelector('button')
-// var main = document.querySelector('main')
-// btn.addEventListener('click', function () {
-//     var div = document.createElement('div')
-
-//     var x = Math.random()*100
-//     var y = Math.random()*100
-//     var r = Math.random()*360
-    
-//     var c1 = Math.floor(Math.random()*256)
-//     var c2 = Math.floor(Math.random()*256)
-//     var c3 = Math.floor(Math.random()*256)
-
-//     div.style.height='50px'
-//     div.style.width='50px'
-//     div.style.position='absolute'
-//     div.style.backgroundColor=`rgb(${c1},${c2},${c3})`
-    
-//     div.style.left=x+'%'
-//     div.style.top=y+'%'
-//     div.style.rotate=r+'deg'
-
-//     main.appendChild(div)
-// })
-
-
-
+var grow = 0
 
 var btn = document.querySelector('button')
-var main = document.querySelector('main')
+var h2 = document.querySelector('h2')
+var p = document.querySelector('p')
+var inner = document.querySelector('.inner')
 
-var arr= ['Hey! I am Rajesh','Sheryians is Best','Learn and Study Ahead','Anubhav is Handsme','Hello Everyone','js is love','Learn everything','Keep practising']
+btn.addEventListener('click', function () {
 
-btn.addEventListener('click',function(){
-    var h1 = document.createElement('h1')
+    btn.style.pointerEvents = 'none'
 
-    var a = Math.floor(Math.random()*arr.length)
-    var x = Math.random()*80
-    var y = Math.random()*80
-    var r = Math.random()*360
-    var scl = Math.random()*3
+    var num = 50 + Math.floor(Math.random() * 50)
 
-    var c1 = Math.floor(Math.random()*256)
-    var c2 = Math.floor(Math.random()*256)
-    var c3 = Math.floor(Math.random()*256)
+    var int = setInterval(() => {
+        grow++
+        h2.innerHTML = grow + '%'
+        inner.style.width = grow + '%'
+        btn.innerHTML = 'Downloading...'
+        btn.style.backgroundColor = 'gray'
 
-    h1.innerHTML=arr[a]
-    
-    h1.style.position='absolute'
-    h1.style.left=x+'%'
-    h1.style.top=y+'%'
-    h1.style.rotate=r+'deg'
-    h1.style.scale=scl
+    }, num);
 
-    // h1.style.color=`rgb(${c1},${c2},${c3})`
+    setTimeout(() => {
+        clearInterval(int)
+        btn.innerHTML = 'Downloaded'
+        btn.style.opacity = 0.5
+        console.log('Downloaded in ' , num/10 , ' seconds');
+        p.innerHTML = 'Download Successfully'
 
-    main.appendChild(h1)
 
+    }, num * 100);
 })
