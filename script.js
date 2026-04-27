396ce2a54faccddde34047181500397ff2a7e88e
Rajesh Maity <rajeshloq2005@gmail.com>
2026-04-27 09:16:18 +0530

    Piano By Keyboard

---
+++ script.js
@@ -0,0 +1,88 @@
+var body = document.querySelector('body')
+var h1 = document.querySelector('h1')
+
+body.addEventListener('keydown', function (dets) {
+    // console.log(dets.code);
+    if (dets.code == 'KeyA') {
+        new Audio("./sounds/21.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyA') {
+        new Audio("./sounds/28.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyB') {
+        new Audio("./sounds/34.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyC') {
+        new Audio("./sounds/40.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyD') {
+        new Audio("./sounds/46.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyE') {
+        new Audio("./sounds/53.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyF') {
+        new Audio("./sounds/66.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyG') {
+        new Audio("./sounds/72.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyH') {
+        new Audio("./sounds/78.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyI') {
+        new Audio("./sounds/84.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyJ') {
+        new Audio("./sounds/90.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyK') {
+        new Audio("./sounds/96.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyL') {
+        new Audio("./sounds/102.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyM') {
+        new Audio("./sounds/108.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyN') {
+        new Audio("./sounds/21.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyO') {
+        new Audio("./sounds/28.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyP') {
+        new Audio("./sounds/34.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyQ') {
+        new Audio("./sounds/40.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyR') {
+        new Audio("./sounds/21.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyS') {
+        new Audio("./sounds/108.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyT') {
+        new Audio("./sounds/28.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyU') {
+        new Audio("./sounds/96.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyV') {
+        new Audio("./sounds/90.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyW') {
+        new Audio("./sounds/84.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyX') {
+        new Audio("./sounds/66.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyY') {
+        new Audio("./sounds/40.mp3").play();
+        h1.innerHTML = dets.key
+    } else if (dets.code == 'KeyZ') {
+        new Audio("./sounds/53.mp3").play();
+        h1.innerHTML = dets.key
+    }
+})
--- task.md
+++ task.md
@@ -1,7 +1,1 @@
-1. make an array of objects and name it users 
-2. users par lagao for-each and print hello n times
-3. make a variable sum and give it a value '' (empty string)
-4. add on 'hello' in sum and print it outside loop
-5. add on card (html code) in sum and print it too.
-6. seect parent (main) and put sum in its innerHTML
-7. replace sample data with real data array of objects
+Task- Piano
