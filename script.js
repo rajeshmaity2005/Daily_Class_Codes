8da9ba7f40295b44da642910fc1cb99828f6c4bb
Rajesh Maity <rajeshloq2005@gmail.com>
2026-05-10 21:33:29 +0530

    Animated-card

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-const para = document.querySelector('p')
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-const text = para.innerText
-
-let iteration = 0;
+var box = document.querySelector("#box")
+box.addEventListener('mouseenter',function(){
 
 
-function randomText() {
-  const str = text.split('').map((char, idx) => {
-    if (idx < iteration) {
-      return char
-    }
-    return characters.split('')[Math.floor(Math.random() * 53)]
-  }).join("")
-  para.innerText = str
-
-  iteration += 0.4;
-}
-
-para.addEventListener('mouseenter', function () {
-
-  setInterval(randomText, 30)
 })
