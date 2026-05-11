65e9f99d126c7e760b621f93af8087d654b2cfca
Rajesh Maity <rajeshloq2005@gmail.com>
2026-05-11 21:54:10 +0530

    Advance CSS Card Animation

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-var box = document.querySelector("#box")
-box.addEventListener('mouseenter',function(){
 
+const h1 = document.querySelector('h1')
+const box = document.querySelector('#box')
+const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+const text = h1.innerText
+let iteration = 0;
+function randomText() {
+  const str = text.split('').map((char, idx) => {
+    if (idx < iteration) {
+      return char
+    }
+    return characters.split('')[Math.floor(Math.random() * 53)]
+  }).join("")
+  h1.innerText = str
 
+  iteration += 0.9;
+}
+
+box.addEventListener('mouseenter', function () {
+  setInterval(randomText, 60)
 })
